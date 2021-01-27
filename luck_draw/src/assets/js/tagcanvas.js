05b443585d950420a62e499b92bd5b356c0fe883
倪刚 <niyongsheng@outlook.com>
2021-01-27 11:58:35 +0800

    [X]. Daily Update

--- luck_draw/src/assets/js/tagcanvas.js
+++ luck_draw/src/assets/js/tagcanvas.js
@@ -1,3 +1,4 @@
+
 /* eslint-disable */
 /**
  * Copyright (C) 2010-2015 Graham Breach
@@ -11,7 +12,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
